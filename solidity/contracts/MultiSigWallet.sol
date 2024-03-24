3eef05bf277c007350d1b87d37d1549465a92be0
0xkyles <kylesyellow@gmail.com>
2024-03-24 14:12:30 +0000

    Added transaction proposal by an owner

--- solidity/contracts/MultiSigWallet.sol
+++ solidity/contracts/MultiSigWallet.sol
@@ -6,6 +6,7 @@ contract MutliSignatureWallet {
   uint256 public numOfOwners; 
   uint256 public immutable approvalsRequired;
 
+  event ProposeTransaction(uint256 indexed txId, address indexed proposer, address indexed to, uint256 value, bytes data);
   event Deposit(address indexed sender, uint256 value);
 
   receive() external payable {
@@ -25,5 +26,37 @@ contract MutliSignatureWallet {
 
     approvalsRequired = _approvalsRequired;
     numOfOwners = _owners.length;
+  }
+
+  struct Transaction {
+    address proposer;
+    address to;
+    uint256 value;
+    bytes data;
+    mapping(address => bool) approvals;
+    uint256 numOfApprovals;
+    bool complete;
+  }
+
+  modifier onlyOwner() {
+    require(owners[msg.sender], "Not owner");
+    _;
+  }
+
+  uint256 public numTransactions;
+  mapping (uint => Transaction) public transactions;
+
+  function proposeTransaction(address _to, uint256 _value, bytes memory _data) public onlyOwner {
+    require(_value <= address(this).balance, "balance insufficient");
+
+    Transaction storage t = transactions[numTransactions++];
+    t.proposer = msg.sender;
+    t.to = _to;
+    t.value = _value;
+    t.data = _data;
+    t.numOfApprovals = 1;
+    t.approvals[msg.sender] = true;
+
+    emit ProposeTransaction(numTransactions - 1, msg.sender, _to, _value, _data);
   }
 }
