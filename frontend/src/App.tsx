8b0ae12bc1bf75380d7ecaf04985086da657d897
0xkyles <kylesyellow@gmail.com>
2024-03-24 16:55:17 +0000

    Add home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card"
+import MetamaskSVG from "./assets/metamask.svg";
+import "./App.css";
 
-function App() {
+export function App() {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <div className="min-h-screen flex justify-center items-center bg-grid">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Connect your wallet</CardTitle>
+          <CardDescription>Access your multi signature wallet now.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button className="w-full">
+            <img className="w-5 h-5 mr-2" src={MetamaskSVG} alt="" />
+            Connect metamask
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+    </>
   )
 }
 
-export default App
+export default App;
